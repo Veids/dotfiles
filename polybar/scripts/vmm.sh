f1a9f5b68d49a003f3eabca1ce533da8b985b67c
Veids <veidsme@gmail.com>
2020-03-01 22:51:13 +0500

    polybar virtual machine/containers monitor

---
+++ polybar/scripts/vmm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Simple script that queries 'lxc ls' and 'virsh list' commands
+
+lxc_icon=
+libvirt_icon=ﲾ
+
+function process_list() {
+    list=("${@:2}")
+    if [ $list ]; then
+        info=""
+        for i in "${!list[@]}"; do
+            info+="$1 ${list[i]} "
+        done
+        echo ${info::-1}
+    fi
+}
+
+# lxc/lxd info
+function get_lxc() {
+    mapfile -t active_list < <(sudo lxc ls --format csv -c n,s | grep RUNNING | awk -F, '{print $1}')
+    process_list $lxc_icon "${active_list[@]}"
+}
+
+# libvirt info includes some vagrant machines using libvirt provider
+function get_libvirt() {
+    mapfile -t active_list < <(sudo virsh list --name 2>/dev/null)
+    process_list $libvirt_icon "${active_list[@]}"
+}
+
+echo $(get_lxc) $(get_libvirt)
